25e6316387322aba4dae51a6e38585356dfea3c3
Şems Turgut <semsmustafaturgut@gmail.com>
2017-08-28 22:22:31 -0500

    The incoming data is parsing process is perfectly fine. We can see changes on mobile app.

--- app/src/main/java/com/tarletonaeronautics/sems/tsurockettrackerforground/MainActivity.java
+++ app/src/main/java/com/tarletonaeronautics/sems/tsurockettrackerforground/MainActivity.java
@@ -1,53 +1,91 @@
 package com.tarletonaeronautics.sems.tsurockettrackerforground;
 
+import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.DoubleBuffer;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private static final int REQUEST_ENABLE_BT = 1;
-
     BluetoothAdapter bluetoothAdapter;
-
     ArrayList<BluetoothDevice> pairedDeviceArrayList;
-
     ListView listViewPairedDevice;
     LinearLayout splashLayout;
-
     ArrayAdapter<BluetoothDevice> pairedDeviceAdapter;
     private UUID myUUID;
     private final String UUID_STRING_WELL_KNOWN_SPP =
             "00001101-0000-1000-8000-00805F9B34FB";
-
     ThreadConnectBTdevice myThreadConnectBTdevice;
     ThreadConnected myThreadConnected;
-
     public String incoming_data;
+
+    SupportMapFragment mapFragment;
+    GoogleMap mGoogleMap;
+    LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    Marker mCurrLocationMarker = null;
+    public double newLatitude = 40.723948, newLongitude = -74.198728;
+    LatLng nLatLng = new LatLng(newLatitude, newLongitude);
+    Button updateCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        updateCamera = (Button) findViewById(R.id.updateCamera);
 
         splashLayout = (LinearLayout) findViewById(R.id.splashLayout);
         listViewPairedDevice = (ListView) findViewById(R.id.pairedlist);
@@ -70,6 +108,14 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
 
+        updateCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder().target(nLatLng).zoom(14).build());
+                mGoogleMap.animateCamera(cameraUpdate);
+            }
+        });
+
     }
 
     @Override
@@ -85,6 +131,17 @@ public class MainActivity extends AppCompatActivity {
         setup();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
     private void setup() {
         Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
         if (pairedDevices.size() > 0) {
@@ -94,6 +151,7 @@ public class MainActivity extends AppCompatActivity {
                 pairedDeviceArrayList.add(device);
             }
 
+            // TODO: Find how to show name on the list.
             pairedDeviceAdapter = new ArrayAdapter<BluetoothDevice>(this,
                     android.R.layout.simple_list_item_1, pairedDeviceArrayList);
             listViewPairedDevice.setAdapter(pairedDeviceAdapter);
@@ -143,10 +201,180 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    private void checkLocationPermission() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Location Permission Needed")
+                        .setMessage("This app needs the Location permission, please accept to use location functionality")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+    /*super.onRequestPermissionsResult(requestCode, permissions, grantResults);*/
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        if (mGoogleApiClient == null) {
+                            buildGoogleApiClient();
+                        }
+                        try {
+                            mGoogleMap.setMyLocationEnabled(true);
+                        } catch (NullPointerException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    //finish();
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        LatLng nLatLng = new LatLng(newLatitude, newLongitude);
+
+        mCurrLocationMarker = mGoogleMap.addMarker(new MarkerOptions()
+                .position(nLatLng)
+                .title("Rocket"));
+
+        //Initialize Google Play Services
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                //Location Permission already granted
+                buildGoogleApiClient();
+                googleMap.setMyLocationEnabled(true);
+            } else {
+                //Request Location Permission
+                checkLocationPermission();
+            }
+        } else {
+            buildGoogleApiClient();
+            googleMap.setMyLocationEnabled(true);
+        }
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //#3213.1646,09812.5574#$
+        //Place current location marker
+//        LatLng nLatLng = new LatLng(newLatitude, newLongitude);
+//
+//        if (mCurrLocationMarker != null) {
+//            mCurrLocationMarker.setPosition(nLatLng);
+//        } else {
+//            mCurrLocationMarker = mGoogleMap.addMarker(new MarkerOptions()
+//                    .position(nLatLng)
+//                    .title("Rocket location")
+//                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+//        }
+//
+
+        //stop location updates
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, (com.google.android.gms.location.LocationListener) this);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
     //Called in ThreadConnectBTdevice once connect successed
     //to start ThreadConnected
     private void startThreadConnected(BluetoothSocket socket) {
-
         myThreadConnected = new ThreadConnected(socket);
         myThreadConnected.start();
     }
@@ -159,7 +387,6 @@ public class MainActivity extends AppCompatActivity {
 
         private BluetoothSocket bluetoothSocket = null;
         private final BluetoothDevice bluetoothDevice;
-
 
         private ThreadConnectBTdevice(BluetoothDevice device) {
             bluetoothDevice = device;
@@ -184,7 +411,6 @@ public class MainActivity extends AppCompatActivity {
 
                 final String eMessage = e.getMessage();
                 runOnUiThread(new Runnable() {
-
                     @Override
                     public void run() {
                         Log.i("bluetoothSocket.connect", eMessage);
@@ -222,20 +448,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void cancel() {
-
             Toast.makeText(getApplicationContext(),
                     "close bluetoothSocket",
                     Toast.LENGTH_LONG).show();
-
             try {
                 bluetoothSocket.close();
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-
         }
-
     }
 
     /*
@@ -274,15 +496,29 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     bytes = connectedInputStream.read(buffer);
                     final String strReceived = new String(buffer, 0, bytes);
-//                    final String strByteCnt = String.valueOf(bytes) + " bytes received.\n";
 
                     runOnUiThread(new Runnable() {
-
                         @Override
                         public void run() {
                             incoming_data += strReceived;
                             if (incoming_data.contains("$")) {
-                                Log.i("Bytes Received", incoming_data);
+                                try {
+//                                    Log.i("Bytes Received", incoming_data);
+                                    String[] dataArray = incoming_data.split(",");
+                                    StringBuilder s = new StringBuilder(dataArray[1]);
+                                    s.insert(2, ".");
+                                    newLatitude = Double.parseDouble(s.toString());
+                                    s = new StringBuilder(dataArray[2]);
+                                    s.insert(3, ".");
+                                    newLongitude = Double.parseDouble(s.toString());
+                                    Log.d("LatLong", String.valueOf(newLatitude + " " + newLongitude));
+                                    nLatLng = new LatLng(newLatitude, newLongitude);
+                                    if (mCurrLocationMarker != null) {
+                                        mCurrLocationMarker.setPosition(nLatLng);
+                                    }
+                                } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                                    e.printStackTrace();
+                                }
                                 incoming_data = "";
                             }
                         }
